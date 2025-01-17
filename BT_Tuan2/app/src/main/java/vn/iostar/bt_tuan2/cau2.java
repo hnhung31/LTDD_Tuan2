1309ae8c0fec98d103282a5031b62bc22ab77ead
Nhung <134117138+hnhung31@users.noreply.github.com>
2025-01-17 10:38:10 +0700

    BT_Tuần 2
    BT_Tuần 2:  Cài đặt môi trường và viết app nhận diện thương hiệu

---
+++ BT_Tuan2/app/src/main/java/vn/iostar/bt_tuan2/cau2.java
@@ -0,0 +1,14 @@
+package vn.iostar.bt_tuan2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class cau2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}
